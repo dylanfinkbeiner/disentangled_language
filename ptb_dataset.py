3c472ef50723c250fc6e62b7e61e038f28ab11a0
LIFE Admiral <ceclifecarpool@gmail.com>
2018-12-31 15:49:32 -0800

    Wrote build_dicts function

--- ptb_dataset.py
+++ ptb_dataset.py
@@ -33,6 +33,14 @@ Probably we will call this once for a
 separate conllu file for the train, dev, and test sets
 '''
 def conllu_to_sents(f: str):
+    '''
+    inputs:
+        f - filename of conllu file
+
+    outputs:
+        sents_list - a list of np arrays with shape (#words-in-sentence, 4)
+
+    '''
 
     mask = [1, 4, 6, 7] # [word, pos, head, rel]
 
@@ -43,18 +51,43 @@ def conllu_to_sents(f: str):
 
     sents_list = []
     sent_start = 0
-        for sent_end in split_points:
-            sents_list.append(lines[sent_start: sent_end])
-            sent_start = sent_end + 1
-    
+    for sent_end in split_points:
+        sents_list.append(lines[sent_start: sent_end])
+        sent_start = sent_end + 1
+
     for i, s in enumerate(sents_list):
-        cleaned = [line.split('\t') for line in s]
-        sents_list[i] = np.array(cleaned)[:, mask] #XXX do we really want them to be np arrays at this point?
+        s_split = [line.split('\t') for line in s]
+        sents_list[i] = np.array(s_split)[:, mask] #XXX do we really want them to be np arrays at this point?
 
     conllu_file.close()
 
     return sents_list
-    
 
 
+def build_dicts(sents):
+
+    words = []
+    pos = []
+    rels = []
+
+    for s in sents:
+        for line in s:
+            words.append(line[0])
+            pos.append(line[1])
+            rels.append(line[3])
+
+    word_vocab = set(words)
+    pos_vocab = set(pos)
+    rels_vocab = set(rels)
+
+    word2num = {w:i for (i,w) in enumerate(word_vocab)}
+    pos2num = {p:i for (i,p) in enumerate(pos_vocab)}
+    rel2num = {r:i for (i,r) in enumerate(rels_vocab)}
+
+    num2word = {i:w for (w,i) in word2num.items()}
+    num2pos = {i:p for (p,i) in pos2num.items()}
+    num2rel = {i:r for (r,i) in rel2num.items()}
+
+
+    return word2num, pos2num, rel2num
 
