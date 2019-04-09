5864ebbad57940d47e8f8883f988a2770b44a373
Dylan M Finkbeiner <dfinkbei@vader.ucsd.edu>
2019-04-09 19:50:38 +0000

    Modified representation losses: concatenating synactic/semantic sub-component of hidden layer

--- src/train.py
+++ src/train.py
@@ -200,15 +200,15 @@ def train(args, parser, data, weights_path=None, exp_path_base=None):
                             loss_paired = loss_h_paired + loss_r_paired
 
                             loss_rep = loss_syn_rep(
-                                    average_hiddens(outputs_batch, batch_lens, device), 
-                                    average_hiddens(outputs_paired, paired_lens, device), 
+                                    average_hiddens(outputs_batch, batch_lens.to(device)),
+                                    average_hiddens(outputs_paired, paired_lens.to(device)), 
                                     scores.to(device), 
                                     syn_size=syn_size,
                                     h_size=h_size)
 
                             loss = loss_batch.to(device) + loss_paired.to(device) + loss_rep
 
-                            train_loss +=  loss_h_batch.item() + loss_r_batch.item() + loss_h_paired.item() + loss_r_paired.item()
+                            train_loss += loss.item()
                             num_steps += 2
                             
 
@@ -262,9 +262,8 @@ def train(args, parser, data, weights_path=None, exp_path_base=None):
             total = 0
             log.info('Evaluation step begins.')
             for b in range(n_dev_batches):
-                log.info(f'Eval batch {b+1}/{n_dev_batches}.')
+                #log.info(f'Eval batch {b+1}/{n_dev_batches}.')
                 with torch.no_grad():
-                    #words, pos, sent_lens, head_targets, rel_targets = next(dev_loader)
                     batch = next(dev_loader)
                     head_targets = batch['head_targets']
                     rel_targets = batch['rel_targets']
@@ -444,11 +443,26 @@ def loss_rels(S_rel, rel_targets, pad_idx=-1):
 
 
 def loss_sem_rep(h1, h2, hn, margin=0.4, syn_size=None, h_size=None):
-    f_para_attract = F.cosine_similarity(h1[:,syn_size:h_size], h2[:,syn_size:h_size]) # (b,2*d), (b,2*d) -> (b)
-    b_para_attract = F.cosine_similarity(h1[:,h_size+syn_size:], h2[:,h_size+syn_size:]) # (b,2*d), (b,2*d) -> (b)
+    '''
+        Based on the loss function from Wieting et al (2018), where the
+        BiLSTM hidden state is treated as a sentence embedding and the goal
+        is to maximize cosine similarity of embeddings of paraphrases
+        and minimize similarity of embeddings of "negative samples".
+    '''
 
-    f_neg_repel = F.cosine_similarity(h1[:,syn_size:h_size], hn[:,syn_size:h_size])
-    b_neg_repel = F.cosine_similarity(h1[:,h_size+syn_size:], hn[:,h_size+syn_size:])
+    #f_para_attract = F.cosine_similarity(h1[:,syn_size:h_size], h2[:,syn_size:h_size]) # (b,sem_size), (b,sem_size) -> (b)
+    #b_para_attract = F.cosine_similarity(h1[:,h_size+syn_size:], h2[:,h_size+syn_size:]) # (b,sem_size), (b,sem_size) -> (b)
+
+    sem_h1 = torch.cat((h1[:,syn_size:h_size], h1[:,h_size+syn_size:]), dim=-1)
+    sem_h2 = torch.cat((h2[:,syn_size:h_size], h2[:,h_size+syn_size:]), dim=-1)
+    sem_hn = torch.cat((hn[:,syn_size:h_size], hn[:,h_size+syn_size:]), dim=-1)
+
+    para_attract = F.cosine_similarity(sem_h1, sem_h2) # (b,sem_size), (b,sem_size) -> (b)
+
+    #f_neg_repel = F.cosine_similarity(h1[:,syn_size:h_size], hn[:,syn_size:h_size]) # (b,2*d), (b,2*d) -> (b)
+    #b_neg_repel = F.cosine_similarity(h1[:,h_size+syn_size:], hn[:,h_size+syn_size:]) # (b,2*d), (b,2*d) -> (b)
+
+    neg_repel = F.cosine_similarity(sem_h1, sem_hn) # (b,sem_size), (b,sem_size) -> (b)
 
     losses = F.relu(margin - para_attract + neg_repel) # (b)
 
@@ -467,16 +481,20 @@ def loss_syn_rep(outputs_batch, outputs_paired, scores, syn_size=None, h_size=No
             losses, where loss for sentence pair (x,y) is 1-cos(x,y) * score(x,y)
     '''
     # All should be (b, l , syn_size) tensors
-    f_batch = outputs_batch[:,0:syn_size]
-    b_batch = outputs_batch[:,h_size:h_size+syn_size]
-    f_paired = outputs_paired[:,0:syn_size]
-    b_paired = outputs_paired[:,h_size:h_size+syn_size]
+    #f_batch = outputs_batch[:,0:syn_size]
+    #b_batch = outputs_batch[:,h_size:h_size+syn_size]
+    #f_paired = outputs_paired[:,0:syn_size]
+    #b_paired = outputs_paired[:,h_size:h_size+syn_size]
+    syn_batch = torch.cat((outputs_batch[:,0:syn_size], outputs_batch[:,h_size:h_size+syn_size]), dim=-1)
+    syn_paired = torch.cat((outputs_paired[:,0:syn_size], outputs_paired[:,h_size:h_size+syn_size]), dim=-1)
 
     # (b)
-    f_loss = 1 - F.cosine_similarity(f_batch, f_paired, dim=-1)
-    b_loss = 1 - F.cosine_similarity(b_batch, b_paired, dim=-1)
+    #f_loss = 1 - F.cosine_similarity(f_batch, f_paired, dim=-1)
+    #b_loss = 1 - F.cosine_similarity(b_batch, b_paired, dim=-1)
 
-    losses = f_loss + b_loss
+    losses = 1 - F.cosine_similarity(syn_batch, syn_paired, dim=-1) 
+
+    #losses = f_loss + b_loss
 
     losses *= scores.view(-1)
 
